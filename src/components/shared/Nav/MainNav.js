abcc070df336ab24b751502fa02e1c4439a5c32d
GinLuskin <reginash12@gmail.com>
2023-02-14 18:02:05 +0200

    main nav&header progress

---
+++ src/components/shared/Nav/MainNav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import MainHeader from "./MainHeader";
+import './MainNav.css';
+
+const MainNavigatio = props => {
+    return <MainHeader>
+        <button className="main-navigation__menu-btn">
+            <span />
+            <span />
+            <span />
+        </button>
+        <h1 className="main-navigation__title">
+            Tasks
+        </h1>
+        <nav>
+            ...
+        </nav>
+    </MainHeader>
+}
